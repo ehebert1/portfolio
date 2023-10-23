dc528d1a30930df8f7a0152df7a53a7d4fecd88c
ehebert1 <ehebman@gmail.com>
2023-10-23 09:18:14 -0400

    feat: Implement locale switching

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ window.onload = () => {
     if (document.cookie.split("; ").find((row) => row.startsWith("lang=")) === undefined) {
         document.cookie = `lang=en;max-age=${7 * 24 * 60 * 60}`
     }
-    const lang = document.cookie
+    lang = document.cookie
         .split("; ")
         .find((row) => row.startsWith("lang="))
         ?.split("=")[1];
@@ -21,6 +21,19 @@ window.onload = () => {
 function updateStrings() {
     document.querySelectorAll("[data-string]")
         .forEach((elm) => {
-            elm.textContent = dataStrings[lang][elm.attributes["data-string"].value]
+            elm.textContent = dataStrings[lang.toLowerCase()][elm.attributes["data-string"].value]
         })
+}
+
+function langSwitch() {
+    assignLangCookie(dataStrings[lang.toLowerCase()]["lang"].toLowerCase())
+    updateStrings()
+}
+
+function assignLangCookie(language) {
+    document.cookie = `lang=${language.toLowerCase()};max-age=${7 * 24 * 60 * 60}`
+    lang = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("lang="))
+        ?.split("=")[1];
 }
